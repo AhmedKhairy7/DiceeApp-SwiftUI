2633111129ab928e604e89dae88108f60675fe8a
ahmedkhairy <ahmedkhairy7111999@yahoo.com>
2022-06-23 19:28:55 +0200

    Initial Commit

---
+++ DiceeApp-SwiftUI/DiceeApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp_SwiftUIApp.swift
+//  DiceeApp-SwiftUI
+//
+//  Created by AhmedKhairy on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
