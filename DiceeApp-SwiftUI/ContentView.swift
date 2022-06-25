dbddb5ae821feda8f7a9a8b44dd69d3e5a1f452b
ahmedkhairy <ahmedkhairy7111999@yahoo.com>
2022-06-25 12:59:56 +0200

    Disce App

--- DiceeApp-SwiftUI/ContentView.swift
+++ DiceeApp-SwiftUI/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Her 2 varibles on to the lift image and one to the right image.
+    @State var lefdiceimage = 1
+    @State var rightdiceimage = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    ExtractedView(n:lefdiceimage)
+                    ExtractedView(n:rightdiceimage)
+                    
+
+
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    // every varible have a 6 random images .
+                    lefdiceimage = Int.random(in: 1...6)
+                    rightdiceimage = Int.random(in: 1...6)
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                }
+                
+            }
+            
+        }
+            
+    }
+        
+}
+struct ExtractedView: View {
+    let n : Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
